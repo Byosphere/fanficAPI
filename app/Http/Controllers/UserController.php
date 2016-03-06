5e1e11d15407f59ae64c48b5111c77e93ddbae22
Byosphere <dreamlike.swarm@gmail.com>
2016-03-06 18:59:48 +0100

    api users
    gestion des utilisateurs par l'api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
 use App\Http\Requests;
+use Validator;
+use Response;
+use Hash;
 
 class UserController extends Controller
 {
@@ -15,7 +18,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        //n'existe pas pour l'api
     }
 
     /**
@@ -25,7 +28,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+        //n'existe pas pour l'api
     }
 
     /**
@@ -36,7 +39,34 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+
+        $regles = array(
+            'name' => 'required|min:5|max:40',
+            'email' => 'required|email',
+            'pass' => 'required|min:5|max:40'
+        );
+
+        $validation = Validator::make($request->all(), $regles);
+
+        if ($validation->fails()) {
+
+            return Response::json(array(
+                'error' => true,
+                'message' => $validation->errors()->all()
+            ));
+
+        } else {
+
+            $user = new User();
+            $user->name = $request->get('name');
+            $user->email = $request->get('email');
+            $user->password = Hash::make($request->get('pass'));
+            $user->save();
+
+            return Response::json(array(
+                'error' => false, 'message' => "Utilisateur enregistré !"));
+
+        }
     }
 
     /**
@@ -47,7 +77,15 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        try {
+
+            $user = User::findOrfail($id);
+            return Response::json(array('error' => false, 'user' => $user->toArray()));
+
+        } catch (\Exception $e) {
+
+            return Response::json(array('error' => true, 'message' => $e->getMessage()));
+        }
     }
 
     /**
@@ -58,7 +96,7 @@ class UserController extends Controller
      */
     public function edit($id)
     {
-        //
+        //n'existe pas pour l'api
     }
 
     /**
@@ -70,7 +108,51 @@ class UserController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+
+        $regles = array(
+            'name' => 'min:5|max:40',
+            'email' => 'email',
+            'pass' => 'min:5|max:40'
+        );
+
+        $validation = Validator::make($request->all(), $regles);
+
+        if ($validation->fails()) {
+
+            return Response::json(array(
+                'error' => true,
+                'message' => $validation->errors()->all()
+            ));
+
+        } else {
+
+            try {
+
+                $user = User::findOrfail($id);
+
+                if($request->get('name'))
+                    $user->name = $request->get('name');
+
+                if($request->get('email'))
+                    $user->email = $request->get('email');
+
+                if($request->get('pass'))
+                    $user->password = Hash::make($request->get('pass'));
+
+                $user->save();
+
+                return Response::json(array(
+                    'error' => false, 'message' => "Utilisateur mis à jour"));
+
+            } catch(\Exception $e) {
+
+                return Response::json(array(
+                    'error' => true,
+                    'message' => $e->getMessage()
+                ));
+            }
+
+        }
     }
 
     /**
@@ -81,6 +163,16 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+
+        try {
+
+            $user = User::findOrfail($id);
+            return Response::json(array('error' => false, 'message' => $user->name+" a bien été supprimé"));
+
+        } catch (\Exception $e) {
+
+            return Response::json(array('error' => true, 'message' => $e->getMessage()));
+        }
+
     }
 }
